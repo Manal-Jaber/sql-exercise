5f2b02e8b5f7ff1c255f3a4e6fe8f90b4e9842aa
Manal-Jaber <manal.jaber94@gmail.com>
2020-10-16 01:50:59 +0300

    Basic queries

---
+++ answers.sh
@@ -0,0 +1,7 @@
+1- SELECT name FROM students
+2- SELECT * FROM students WHERE students.Age >30
+3- SELECT name FROM students WHERE (students.Gender= "F" AND students.Age =30)
+4- SELECT Points FROM students WHERE students.name = "Alex"
+5- INSERT INTO students (name, Age, Gender, Points) VALUES("Manal", 26, "F", 300)
+6- UPDATE students SET Points = 400 WHERE students.Name = "Basma"
+7- UPDATE students SET Points = 190 WHERE students.Name = "Alex"
